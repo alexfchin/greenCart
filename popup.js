a4970892c791c28f07fbebb266f83307d49a2eb4
richelle246@gmail.com <32554353+richellec@users.noreply.github.com>
2019-03-30 15:04:03 -0400

    if statements for ratingicon

--- popup.js
+++ popup.js
@@ -3,5 +3,17 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     document.getElementById('productBrand').textContent=response.productBrand;
     document.getElementById('productName').textContent=response.productName;
     document.getElementById('productImage').src=response.productImage;
-  });
+
+
+    if (GRADE == "A"){
+      document.getElementById('ratingIcon').src="images/ratingA.png";
+    }else if (GRADE == "B") {
+      document.getElementById('ratingIcon').src="images/ratingB.png";
+    }else if (GRADE == "C") {
+      document.getElementById('ratingIcon').src="images/ratingC.png";
+    }else if(GRADE == "D"){
+      document.getElementById('ratingIcon').src="images/ratingD.png";
+    }else if (GRADE == "F"){
+      document.getElementById('ratingIcon').src="images/ratingF.png";
+    }
 });
