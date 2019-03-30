4ef08920786a0fb3d3857fc962eb932d1fa198a7
Yang Yang <yangyang1998@gmail.com>
2019-03-30 05:07:43 -0400

    preliminary flask server

---
+++ database.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from google.cloud import storage
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print('File {} uploaded to {}.'.format(
+        source_file_name,
+        destination_blob_name))
+
+app = Flask(__name__)
+
+@app.rout("/")
+def hello():
+    return "Hello World!"
+
--- scraper.py
+++ scraper.py
@@ -1,9 +1,17 @@
-import scrapy
+from flask import Flask
+from flask import request
+import scrapy #TODO: Why not beautiful soup?
 #scrapy runspider scraper.py
+
+app = Flask(__name__)
 
 class BrickSetSpider(scrapy.Spider):
     name = 'spider'
-    start_urls = ['https://www.amazon.com/dp/B07K7G74P6/ref=br_msw_pdt-4?_encoding=UTF8&smid=AP7ZCPRBBY481&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=N8ND6THGHBZ1AP9HW0VQ&pf_rd_t=36701&pf_rd_p=d8db102a-01f2-4609-b58d-14bd5bbc3e8c&pf_rd_i=desktop']
+    start_urls = []
+#    start_urls = ['https://www.amazon.com/dp/B07K7G74P6/ref=br_msw_pdt-4?_encoding=UTF8&smid=AP7ZCPRBBY481&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=N8ND6THGHBZ1AP9HW0VQ&pf_rd_t=36701&pf_rd_p=d8db102a-01f2-4609-b58d-14bd5bbc3e8c&pf_rd_i=desktop']
+    def __init__(self, product_url):
+        self.start_urls.append(product_url)
+
     def parse(self, response):
         byinfoName = ''
         SET_SELECTOR = '#bylineInfo'
@@ -15,4 +23,21 @@ class BrickSetSpider(scrapy.Spider):
             byinfoName += tag.css(NAME_SELECTOR).extract_first()
         print(byinfoName.capitalize())
         return byinfoName
+
+def GetCompany(product):
+    # something
+
+def GetCompanyEmissions(company):
+    # other thing
+
+@app.route('/product', methods=['GET'])
+def GetProductInfo():
+    url = request.args.get('url')
+    scrape = BrickSetSpider(url)
+
+    #TODO: Where does response come from?
+    response = []
+    product_name = scrape.parse(response)
+    company = GetCompany(product_name)
+    return GetCompanyEmissions(return)
 
