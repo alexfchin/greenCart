50decf15a66a733ac29693b07760dd514ff57639
Yang Yang <yangyang1998@gmail.com>
2019-03-30 13:06:50 -0400

    nvm, issue lied with amazon smile links

--- extension/popup.js
+++ extension/popup.js
@@ -1,23 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-	let link = document.getElementById('button');
-	// onClick's logic below:
-	link.addEventListener('click', function() {
-		// Chrome tab stuff
-		chrome.tabs.getSelected(null, function(tab) {
-			var tabId = tab.id;
-			var tabUrl = tab.url;
-
-			document.getElementById('productName').textContent=tabUrl;
-		});
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+		document.getElementById('productBrand').textContent=response.productBrand;
+		document.getElementById('productName').textContent=response.productName;
 	});
 });
-
-$(document).ready(function() {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-			document.getElementById('productBrand').textContent=response.productBrand;
-			document.getElementById('productName').textContent=response.productName;
-		});
-	});
-});
-
