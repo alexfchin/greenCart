6ec53e3b882c8a8f61a3a9ff5dfdb50683c2f12a
richelle246@gmail.com <32554353+richellec@users.noreply.github.com>
2019-03-30 12:41:02 -0400

    brand, product name, image, and sizing

--- popup.js
+++ popup.js
@@ -2,5 +2,6 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
     document.getElementById('productBrand').textContent=response.productBrand;
     document.getElementById('productName').textContent=response.productName;
+    document.getElementById('productImage').src=response.productImage;
   });
 });
